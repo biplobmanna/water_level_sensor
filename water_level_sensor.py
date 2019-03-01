9311d0dffceb13c8c256ed2f55d84d7fdf37b359
biplobmanna <biplobmanna@outlook.com>
2019-03-01 08:20:17 +0000

    Modifying code to implement additional sensor operations.

--- water_level_sensor.py
+++ water_level_sensor.py
@@ -7,13 +7,19 @@ from logger import wls_logger as log
 
 # initialise all the global variables
 def init_all():
-	global distance, motor_running_status
+	global distance, motor_running_status, sump_present, distance_sump
+	# Setting the motor as off, for safety purposes
+	sio.set_motor_switch(False)
 	log.debug("\n\nStarted at: "+ str(time.ctime()))
 	try:
 		# Fetching initial distance from FB-DB
 		distance = fio.get_distance()
 		# Fetch initial Motor running status from FB-DB
 		motor_running_status = fio.is_motor_switch_on()
+		# Fetching initial distance_sump from FB-DB
+		distance_sump = fio.get_distance_sump()
+		# Fetching initial data to check if sump is present or not
+		sump_present = fio.is_sump_present()
 	except:
 		print('Error fetching data from Firebase!')
 		log.debug('Error fetching data from Firebase!')
@@ -23,46 +29,93 @@ def init_all():
 		log.debug('Initial distance fetched from FB-DB!')
 		print('Initial Motor running status fetched from FB-DB!')
 		log.debug('Initial Motor running status fetched from FB-DB!')
+		print('Initial distance_sump fetched from FB-DB!')
+		log.debug('Initial distance_sump fetched from FB-DB!')
+		print('Success fetching sump present or not from FB-DB!')
+		log.debug('Success fetching sump present or not from FB-DB!')
 		return True
 
-# Running the entire mechanism
-def run_mechanism():
-	global distance, motor_running_status
-	
+# Main Switch Operation
+def main_switch_op():
 	try:
 		# Checking main_switch status
 		if not fio.is_main_switch_on():
-			return
+			return False
 	except:
 		print('Error fetching main_switch status from Firebase!')
 		log.debug('Error fetching main_switch status from Firebase!')
-		return
-	
+		return False
+	return True
+
+# Distance Operation
+def distance_op():
+	global distance
 	try:
 		# Read the new distance
-		new_distance = sio.read_distance()
+		#new_distance = sio.read_distance()
+		# For testing
+		new_distance = int(raw_input("Enter distance:"))
 	except:
-		print('Error reading data from the sensor!')
-		log.debug('Error reading data from the sensor!')
-		return
+		print('Error reading distance data from the sensor!')
+		log.debug('Error reading distance data from the sensor!')
+		return False
 	
 	# Check if the new_distance read is different from the old data
 	if new_distance != distance:
 		distance = new_distance
 	else:
-		return
+		return True
 	
+	# Set the new distance in FB-DB
 	try:
 		# Set the distance in FB DB
 		fio.set_distance(distance)
 	except:
 		print('Error setting the distance in Firebase!')
 		log.debug('Error setting the distance in Firebase!')
-		return
-	
+		return True
+	return True
+
+# Distance Sump Operation
+def distance_sump_op():
+	global distance_sump, sump_present
+	if not fio.is_sump_present():
+		return True
+	try:
+		new_distance_sump = sio.read_distance_sump()
+	except:
+		print('Error reading distance_sump from the sensor!')
+		log.debug('Error reading distance_sump from the sensor!')
+		return False
+	# Check if the new_distance_sump is different from old data
+	if new_distance_sump != distance_sump:
+		distance_sump = new_distance_sump
+	else:
+		return True
+	# Set the new distance_sump in FB-DB
+	try:
+		fio.set_distance_sump(distance_sump)
+	except:
+		print('Error setting distance_sump into firebase!')
+		log.debug('Error setting distance_sump into firebase!')
+		return True
+	return True
+
+# Setting Motor Running Status Operation
+def motor_run_op():
+	global distance, distance_sump, motor_running_status, sump_present
 	# Set the motor running status depending on the distance threshold
 	new_motor_running_status = sio.is_distance_over_threshold(distance, motor_running_status)
 	
+	# Check if sump present or not, if present, checking if water present or not
+	if sump_present:
+		sump_water_present = not sio.is_distance_sump_over_threshold(distance_sump)
+	else:
+		sump_water_present = True
+	
+	# Doing a boolean operation of new_motor_running_status and sump_water_present
+	new_motor_running_status = new_motor_running_status and sump_water_present
+
 	# Check if the new motor status is different from the old one
 	if new_motor_running_status != motor_running_status:
 		motor_running_status = new_motor_running_status
@@ -72,7 +125,34 @@ def run_mechanism():
 		except:
 			print('Error setting the motor running status in Firebase')
 			log.debug('Error setting the motor running status in Firebase')
-			return
+	return motor_running_status
+
+# Running the entire mechanism
+def run_mechanism():
+	# If h/w switch is off, switch off motor
+	# Perform no operation no-matter what be the case
+	if not sio.read_normal_switch():
+		sio.set_motor_switch(False)
+		return
+	
+	# If main_switch is off, switch off motor
+	if not main_switch_op():
+		sio.set_motor_switch(False)
+		return
+	
+	# If distance operation has any error, set off motor
+	if not distance_op():
+		sio.set_motor_switch(False)
+		return
+	
+	# If sump distance operation has any error, set off motor
+	if not distance_sump_op():
+		sio.set_motor_switch(False)
+		return
+	
+	motor_running_status = motor_run_op()
+	# Set motor running status to motor switch
+	sio.set_motor_switch(motor_running_status)
 
 # Main run function
 # Only need to call it and then leave it alone
