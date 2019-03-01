3a1c79ef5e5737fc288397102e982255b7ea538a
biplobmanna <biplobmanna@outlook.com>
2019-03-01 09:58:48 +0000

    Modifying install.sh to install all the newly created files!

--- install.sh
+++ install.sh
@@ -59,15 +59,45 @@ else
 	echo ">> water_level_sensor.py copied to /home/pi/Projects/WaterLevelSensor/"
 fi
 
-# Checking if the firebase_link python file is present or not
-if [ -f /home/pi/Projects/WaterLevelSensor/firebase_link.py ]; then
-	echo "** File firebase_link is present!"
+# Checking if the sensor_io file is present or not
+if [ -f "/home/pi/Projects/WaterLevelSensor/sensor_io.py" ]; then
+	echo "** sensor_io.py exists!"
+else
+	sudo cp sensor_io.py /home/pi/Projects/WaterLevelSensor/
+	echo ">> sensor_io.py copied to /home/pi/Projects/WaterLevelSensor/"
+fi
+
+# Checking if the firebase_io file is present or not
+if [ -f "/home/pi/Projects/WaterLevelSensor/firebase_io.py" ]; then
+	echo "** firebase_io.py exists!"
+else
+	sudo cp firebase_io.py /home/pi/Projects/WaterLevelSensor/
+	echo ">> firebase_io.py copied to /home/pi/Projects/WaterLevelSensor/"
+fi
+
+# Checking if the logger file is present or not
+if [ -f "/home/pi/Projects/WaterLevelSensor/logger.py" ]; then
+	echo "** logger.py exists!"
+else
+	sudo cp logger.py /home/pi/Projects/WaterLevelSensor/
+	echo ">> logger.py copied to /home/pi/Projects/WaterLevelSensor/"
+fi
+
+
+# Checking if the firebase_link configuration file is present or not
+if [ -f /home/pi/Projects/WaterLevelSensor/firebase_link.conf ]; then
+	echo "** File firebase_link configuration file is present!"
 else
 	# Add Firebase link either in this file or in the created file
 	# Add the link without the trailing '/'
-	echo "FIREBASE_PATH=\"add_your_link_here_without_the_last_/\"" > /home/pi/Projects/WaterLevelSensor/firebase_link.py
-	echo ">> Created firebase_link.py in /home/pi/Projects/WaterLevelSensor/ path!"
-	echo "!!!! Add your firebase link to 'firebase_link.py' to ** FIREBASE_PATH ** variable !!!!"
-	echo "!!!! Remove the last trailing '/' for the link to work !!!!"
+	touch /home/pi/Projects/WaterLevelSensor/firebase_link.conf
+	echo ">> Created firebase_link.conf in /home/pi/Projects/WaterLevelSensor/ path!"
 fi
 
+# Show warning related to set the firebase-database link
+echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+echo "!!!!                                                                              !!!!"
+echo "!!!! Add your firebase database link in the first line of firebase_link.conf file !!!!"
+echo "!!!! Remove the last trailing '/' for the link to work!                           !!!!"
+echo "!!!!                                                                              !!!!"
+echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
